2e788b253915902ef70cab6feab1ac97d14aca3d
jiesihu <jiesihu@usc.edu>
2022-04-04 11:40:24 +0800

    add file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='esender',
+      version='0.1.0',
+      description='send email for notification',
+      url='http://github.com/tongling/clinicaltrial',
+      author='Jiesi',
+      author_email='jiesihu@usc.com',
+      license='MIT',
+      packages=['esender'],
+      zip_safe=False)
