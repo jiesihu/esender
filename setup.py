0cb0ecc21411353349b1a59119d7fc24b3420c7c
jiesihu <jiesihu@usc.edu>
2022-04-04 12:31:36 +0800

    add function

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup
 setup(name='esender',
       version='0.1.0',
       description='send email for notification',
-      url='http://github.com/tongling/clinicaltrial',
+      url=' ',
       author='Jiesi',
       author_email='jiesihu@usc.com',
       license='MIT',
       packages=['esender'],
+      install_requires=[
+            'yagmail>=0.15.277',  #所需要包的版本号
+            ],
       zip_safe=False)
